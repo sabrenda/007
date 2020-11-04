36eeedc7681d82b031df5a5f82d9ce60f8a12e62
sabrenda <nob_32@mail.ru>
2020-11-04 05:56:35 +0300

    111

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -10,6 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	int		i;
@@ -17,22 +19,23 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		len;
 	char	*str;
 
-	if (!s1 && !s2)
+	if (!s1 || !s2)
 		return (NULL);
 	len = ft_strlen(s1) + ft_strlen(s2);
-	if (!(str = (char *)malloc(sizeof(s1) * (len + 1))))
+	if (!(str = (char *)malloc(sizeof(s1) * len )))
 		return 	(NULL);
 		i = 0;
 		j = 0;
-	while (i < ft_strlen(s1))
+	while (s1[i])
 	{
 		str[i] = s1[i];
 		i++;
 	}
-	while (j < ft_strlen(s2))
+	while (s2[j])
 	{
-		str[i] = s1[j];
+		str[i] = s2[j];
 		i++;
+		j++;
 	}
 	str[i] = '\0';
 	return (str);
--- libft/libft.h
+++ libft/libft.h
@@ -42,6 +42,6 @@ int		ft_tolower(int ch);
 void	*ft_calloc(size_t num, size_t size);
 char	*ft_strdup(const char *str);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-char    *ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
