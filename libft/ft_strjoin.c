b923270c1224f7e3a18f2dda2222df3821fc131f
sabrenda <nob_32@mail.ru>
2020-11-04 05:17:52 +0300

    111

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -10,18 +10,30 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strjoinn(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
 	int		i;
 	int		j;
+	int		len;
 	char	*str;
 
 	if (!s1 && !s2)
 		return (NULL);
 	len = ft_strlen(s1) + ft_strlen(s2);
-	if(!(str = (char *)malloc(sizeof(s1) * (len + 1))))
+	if (!(str = (char *)malloc(sizeof(s1) * (len + 1))))
 		return 	(NULL);
-
-
-
+		i = 0;
+		j = 0;
+	while (i < ft_strlen(s1))
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (j < ft_strlen(s2))
+	{
+		str[i] = s1[j];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
