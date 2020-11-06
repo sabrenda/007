6c2a0c2f0d76aea9a016ad79a7c06c6a84083d3b
sabrenda <nob_32@mail.ru>
2020-11-06 22:50:31 +0300

    0611201050

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: sabrenda <sabrenda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/28 16:02:19 by sabrenda          #+#    #+#             */
-/*   Updated: 2020/11/05 03:11:17 by sabrenda         ###   ########.fr       */
+/*   Updated: 2020/11/06 19:52:03 by sabrenda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,5 +44,8 @@ char	*ft_strdup(const char *str);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
 #endif
