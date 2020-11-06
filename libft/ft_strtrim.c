355b61b9d2c9c3a476256f5f7bd43476b3823544
sabrenda <nob_32@mail.ru>
2020-11-06 18:10:30 +0300

    0611200610

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: sabrenda <sabrenda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/05 01:26:28 by sabrenda          #+#    #+#             */
-/*   Updated: 2020/11/05 03:09:22 by sabrenda         ###   ########.fr       */
+/*   Updated: 2020/11/06 18:09:44 by sabrenda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,6 @@ char	*ft_strtrim(char const *s1, char const *set)
 	if (s1[start] == '\0' || ft_strlen(s1) == 0)
 		return (ft_strdup(""));
 	while (ft_strchr(set, s1[end]) != 0)
-			end--;
+		end--;
 	return (ft_substr(s1, start, (end - start + 1)));
 }
